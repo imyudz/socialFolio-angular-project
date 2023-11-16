7e7ecc927badc50b670b69c990e207bb9b57c01a
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-11-16 00:39:32 -0300

    feat: Adicionando componentes post-section e perfil sidebar

---
+++ src/app/components/perfil-sidebar/perfil-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-sidebar',
+  templateUrl: './perfil-sidebar.component.html',
+  styleUrls: ['./perfil-sidebar.component.css']
+})
+export class PerfilSidebarComponent {
+
+}
