09e314122c4c547a2b9fd1b0ec8e136d3ae3dbc9
Yan <yan1alves@gmail.com>
2023-11-17 19:19:12 -0300

    Adicionando imagem de perfil

--- src/app/pages/perfil-section/perfil-section.component.ts
+++ src/app/pages/perfil-section/perfil-section.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-perfil-section',
   templateUrl: './perfil-section.component.html',
   styleUrls: ['./perfil-section.component.css']
 })
 export class PerfilSectionComponent {
+  imgProfile: any = '../../../assets/imgProfile/imgProfile.png'; 
 
 }
