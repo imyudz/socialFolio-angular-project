bb4dbe77a0f610ec0e61a7d860548f0ef3a02bba
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-11-20 15:50:48 -0300

    feat e refactor: Alterando estrutura de pastas para melhor organização. Adicionando serviço de autenticação e protetor de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HeaderComponent } from './components/header/header.component';
-import { PostSectionComponent } from './pages/post-section/post-section.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-
-import { PostComponent } from './components/post/post.component';
-import { CurriculoSectionComponent } from './pages/curriculo-section/curriculo-section.component';
-import { PerfilSidebarComponent } from './components/perfil-sidebar/perfil-sidebar.component';
-import { PerfilSectionComponent } from './pages/perfil-section/perfil-section.component';
-import { ProfileSideCardComponent } from './components/profile-side-card/profile-side-card.component';
-import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
-
+import { SharedModule } from './shared/shared.module';
+import { PostSectionModule } from './pages/post-section/post-section.module';
+import { CurriculoSectionModule } from './pages/curriculo-section/curriculo-section.module';
+import { PerfilSectionModule } from './pages/perfil-section/perfil-section.module';
+import { LoginModule } from './pages/login/login.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    PostSectionComponent,
-
-    PostComponent,
-    CurriculoSectionComponent,
-    PerfilSidebarComponent,
-    PerfilSectionComponent,
-    ProfileSideCardComponent,
-    ProfileInfoComponent
+    AppComponent
   ],
 
   imports: [
     BrowserModule,
     FontAwesomeModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    PostSectionModule,
+    CurriculoSectionModule,
+    PerfilSectionModule,
+    LoginModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(public authService: AuthService) { }
+
+  private username: string = "teste@123.com";
+  private password: string = "teste";
+
+
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.authService.login(this.username, this.password);
+  }
+}
