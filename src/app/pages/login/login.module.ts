da7a641e564e2a938c9d6bd73c22dbbb105c235d
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2023-11-21 00:09:32 -0300

    adicionando mudanças em registro component

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -14,7 +15,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     SharedModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ]
 })
 export class LoginModule { }
